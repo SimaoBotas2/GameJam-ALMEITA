d6b8554578785a133c2d1f1e4038d1079be93d69
SimaoBotas2 <simaotomas9@gmail.com>
2026-04-18 14:59:08 +0100

    Interação com objetos pseudo pronta

--- virus/scripts/sigma_character.gd
+++ virus/scripts/sigma_character.gd
@@ -1,18 +1,20 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 350
-@export var starting_direction: Vector2 = Vector2.ZERO #probably gonna change
+@export var starting_direction: Vector2 = Vector2.ZERO
+
+var current_interaction_object: Area2D = null
 
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
+@onready var interaction_prompt = $InteractionPrompt
 
-#not necessary as long as the idle animation is just one
 func _ready():
 	animation_tree.active = true
+	interaction_prompt.visible = false
 	update_animation_parameters(starting_direction)
 
 func _physics_process(_delta):
-	#Get input diretion
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
@@ -23,17 +25,28 @@ func _physics_process(_delta):
 	
 	update_animation_parameters(input_direction)
 	pick_new_state()
+	handle_interaction()
 
-#animation in function of move_input
 func update_animation_parameters(move_input: Vector2):
 	if move_input != Vector2.ZERO:
 		animation_tree.set("parameters/Walk/blend_position", move_input)
-		#animation_tree.set("parameters/Idle/blend_position", move_input) 
-		#not necessary as long as the idle animation is just one
 
-#animation in function of the velocity
 func pick_new_state():
 	if velocity != Vector2.ZERO:
 		state_machine.travel("Walk")
 	else:
 		state_machine.travel("Idle")
+
+func handle_interaction() -> void:
+	if current_interaction_object != null and Input.is_action_just_pressed("interact"):
+		print("Placeholder: interação com ", current_interaction_object.name)
+
+func _on_interaction_detector_area_entered(area: Area2D) -> void:
+	if area.is_in_group("interaction_object"):
+		current_interaction_object = area
+		interaction_prompt.visible = true
+
+func _on_interaction_detector_area_exited(area: Area2D) -> void:
+	if area == current_interaction_object:
+		current_interaction_object = null
+		interaction_prompt.visible = false
